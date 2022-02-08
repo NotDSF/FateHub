9db6ae0b3ab99f02526fb380ea210b5277bed16e
NotDSF <67711164+NotDSF@users.noreply.github.com>
2022-02-08 21:16:18 +0000

    Jailbreak v1.0.1c
    [+]: Added visuals.
    [*]: Fixed ninja mode.

--- Jailbreak.lua
+++ Jailbreak.lua
@@ -1,3 +1,4 @@
+local Teams = game:GetService("Teams")
 -- The types are just to help me with remembering the names
 
 type ChassisType = {
@@ -173,12 +174,6 @@ end;
 
 for i,v in pairs(Keys) do
     rconsoleinfo(string.format("Automatically grabbed %s key: %s\n", i, v));
-end;
-
-local BackupChassis = Chassis.UpdatePrePhysics;
-Chassis.UpdatePrePhysics = function(...) 
-    if Flags.VehicleChassisDisable then return end;
-    return BackupChassis(...);
 end;
 
 for i,v in pairs(Getgc(true)) do
@@ -705,6 +700,86 @@ local GetClosestPlayer; do
     end;
 end;
 
+-- Visuals 
+local ESP = {}; do 
+    local ESPConfig = {} do
+        local RDynamic  = RectDynamic
+        local PointInst = PointInstance;
+    
+        ESPConfig = {
+            Box = {
+                Thickness = 1,
+                Color = Color3.new(0.364705, 0.992156, 0.305882);
+                EnemyColor = Color3.new(0.972549, 0.266666, 0.266666);
+            },
+            Tracers = {}
+        }
+    
+        local Objects = {};
+        function ESP:AddBox(object, team) 
+            local Point1 = PointInst.new(object, CFrame(Vector3(2.5, 3, 0)));
+            Point1.RotationType = CFrameRotationType.CameraRelative
+
+            local Point2 = PointInst.new(object, CFrame(Vector3(-2.5, -3.5, 0)));
+            Point2.RotationType = CFrameRotationType.CameraRelative
+
+            local Rect = RDynamic.new(Point1, Point2);
+    
+            Rect.Thickness = ESPConfig.Box.Thickness;
+            Rect.Color = ESPConfig.Box[team and "Color" or "EnemyColor"];
+    
+            Objects[object] = { Object = Rect, Team = team };
+        end;
+    
+        function ESP:RemoveAll() 
+            for i,v in Pairs(Objects) do 
+                v.Object.Visible = false;
+            end;
+        end;
+
+        function ESP:RenderLoop() 
+            for i, v in Pairs(Objects) do
+                v.Object.Thickness = ESPConfig.Box.Thickness;
+                v.Object.Color = ESPConfig.Box[v.Team and "Color" or "EnemyColor"];
+            end;
+        end;
+    end;
+
+    local ESPUI = UI.New({
+        Title = "Visuals"
+    });
+
+    ESPUI.Toggle({
+        Text = "Box ESP",
+        Callback = function(bool) 
+            if bool then
+                for i,v in Pairs(GetChildren(game.Players)) do
+                    if v ~= LocalPlayer and v.Character and FindFirstChild(v.Character, "HumanoidRootPart") then
+                        ESP:AddBox(v.Character.HumanoidRootPart, v.TeamColor == LocalPlayer.TeamColor);
+                    end;
+                end;
+            end;
+            Flags.BoxESP = bool;
+        end;
+    });
+
+    ESPUI.ColorPicker({
+        Text = "Team Color",
+        Default = ESPConfig.Box.Color,
+        Callback = function(value) 
+            ESPConfig.Box.Color = value;
+        end;
+    });
+
+    ESPUI.ColorPicker({
+        Text = "Enemy Color",
+        Default = ESPConfig.Box.EnemyColor,
+        Callback = function(value) 
+            ESPConfig.Box.EnemyColor = value;
+        end;
+    });
+end;
+
 Vehicle.OnVehicleEntered:Connect(function(Vehicle) 
     warn("debug: entered vehicle");
 
@@ -748,6 +823,10 @@ Item.OnLocalItemUnequipped:Connect(function()
 end);
 
 game.RunService.Stepped:Connect(function() 
+    if Flags.BoxESP then
+        ESP:RenderLoop();
+    end;
+
     if Flags.Aimbot and IsMouseButtonPressed(UserInputService, Enum.UserInputType.MouseButton2) and Flags.ItemEquipped then
         local Closest = GetClosestPlayer();
         if Closest then
@@ -777,7 +856,7 @@ game.RunService.Stepped:Connect(function()
                 if not Vehicle.BackupLift then
                     SynapseNotification("Ninja mode activated.");
                     Vehicle.BackupLift = Vehicle.Lift.Force;
-                    Vehicle.Lift.Force = Vector3.new(0, 999999, 0);
+                    Vehicle.Lift.Force = Vector3(0, 999999, 0);
                 end;
             elseif Vehicle.BackupLift then
                 Vehicle.Lift.Force = Vehicle.BackupLift;
