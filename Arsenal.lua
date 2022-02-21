ca257d441a80bac69a9369206e67c410aa0118f8
NotDSF <67711164+NotDSF@users.noreply.github.com>
2022-02-21 01:16:53 +0000

    Arsenal rewrite

--- Arsenal.lua
+++ Arsenal.lua
@@ -4,15 +4,22 @@ getgenv().FatesHub = true;
 
 ToastType = ToastType or {};
 
+type Main = {
+    Toggle: (self: Main, name: string, default: boolean?, callback: (value: boolean) -> nil) -> nil,
+    Dropdown: (self: Main, name: string, start: string, list: {}, callback: (selected: string) -> nil) -> nil,
+    Slider: (self: Main, name: string, min: number, max: number, default: number, callback: (value: number) -> nil, nofill: boolean?, floor: boolean?) -> nil,
+    Colorpicker: (self: Main, name: string, StartingColor: Color3, callback: (color: Color3) -> nil) -> nil,
+    Keybind: (self: Main, name: string, StartingKey: Enum.KeyCode?, onset: (keycode: Enum.KeyCode) -> nil, oninput: () -> nil) -> nil,
+    Button: (self: Main, name: string, callback: () -> nil) -> nil
+}
+
+type Section = { Section: (self: Section, name: string) -> Main }
+type Window = { Tab: (self: Window, name: string) -> Section }
+type Library = { CreateWindow: (self: Library, name: string, game: string) -> Window }
+
 local TNow = tick();
-local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))();
-local UI = Lib.Load({
-    Title = "Fates Hub",
-    Style = 1,
-    SizeX = 500,
-    SizeY = 600,
-    Theme = "Dark"
-});
+local Lib: Library = loadstring(readfile("UILib.lua"))();
+local Window = Lib:CreateWindow("FH", "Arsenal");
 
 local Rawget = rawget;
 local Type = typeof;
@@ -35,6 +42,7 @@ local RaycastParams = RaycastParams.new;
 local CFrame = CFrame.new;
 local sort = table.sort;
 local info = debug.info;
+local Delay = task.delay;
 local MouseButton2 = Enum.UserInputType.MouseButton2;
 local Checkcaller = checkcaller;
 local Flags = {};
@@ -77,16 +85,23 @@ SynapseNotification("ALERT: You are running a beta version of Fates Hub.\nPlease
 local Variables = GetLocal("primarystored");
 local Crouch = GetLocal("ctrlcrouch");
 local Weapon = GetLocal("firebullet");
+local ModCheck = GetLocal("isMod");
 local RotCamera = GetFunction("RotCamera");
 local ammocount = Variables.ammocount;
+local equipped = Variables.equipped;
+local currentspread = GetLocal("currentspread").currentspread;
 
 BackupIndex = hookmetamethod(Game, "__index", newcclosure(function(self, idx) 
     if idx == "WalkSpeed" then
         return 16;
     elseif idx == "JumpPower" then
         return 50;
-    elseif Flags.InfAmmo and self == ammocount and idx == "Value" then
+    elseif Flags.InfiniteAmmo and self == ammocount and idx == "Value" then
         return 100;
+    elseif Flags.FieldOfView and self == Camera and idx == "FieldOfView" then
+        return Flags.FieldOfView;
+    elseif Flags.NoSpread and self == currentspread and idx == "Value" then
+        return 0;
     end;
     return BackupIndex(self, idx);
 end));
@@ -104,184 +119,178 @@ BackupRot = hookfunction(RotCamera, function(...)
     return BackupRot(...);
 end);
 
--- Player
-do 
-    local Player = UI.New({
-        Title = "Player"
-    });
+local function GetClosestPlayer(novisible) 
+    if not LocalPlayer.Character or not FindFirstChild(LocalPlayer.Character, "HumanoidRootPart") then return end;
 
-    Player.Slider({
-        Text = "WalkSpeed",
-        Min = 1,
-        Def = 16,
-        Max = 200,
-        Callback = function(value) 
-            LocalPlayer.Character.Humanoid.WalkSpeed = value;
-            Flags.WalkSpeed = value;
-        end
-    });
+    local LPos = LocalPlayer.Character.HumanoidRootPart.Position;
+    local Players = {};
 
-    Player.Slider({
-        Text = "JumpPower",
-        Min = 1,
-        Def = 50,
-        Max = 200,
-        Callback = function(value) 
-            LocalPlayer.Character.Humanoid.JumpPower = value;
-            Flags.JumpPower = value;
-        end;
-    });
-end;
+    for i,v in Pairs(GetChildren(GPlayers)) do
+        if v ~= LocalPlayer then
+            if Flags.TeamCheck and v.TeamColor == LocalPlayer.TeamColor then 
+                continue;
+            end;
 
--- Combat
-do 
-    local Combat = UI.New({
-        Title = "Combat"
-    });
-
-    Combat.Toggle({
-        Text = "Infinite Ammo",
-        Callback = function(bool) 
-            Flags.InfAmmo = bool;
-        end,
-        Enabled = false
-    });
-
-    Combat.Toggle({
-        Text = "No Recoil",
-        Callback = function(bool) 
-            Flags.NoRecoil = bool;
-        end;
-    });
-
-    Combat.Toggle({
-        Text = "Trigger Bot",
-        Callback = function(bool) 
-            Flags.Triggerbot = bool;
-        end;
-    });
-
-    Combat.Toggle({
-        Text = "Rage Bot",
-        Callback = function(bool) 
-            Flags.Ragebot = bool;
-        end;
-    });
-
-    Combat.Toggle({
-        Text = "Auto Crouch",
-        Callback = function(bool) 
-            Flags.AutoCrouch = bool;
-        end;
-    });
-end;
-
--- Aimbot
-local GetClosestPlayer; do 
-    local Aimbot = UI.New({
-        Title = "Aimbot"
-    });
-
-    Aimbot.Toggle({
-        Text = "Enabled",
-        Callback = function(bool) 
-            Flags.Aimbot = bool;
-        end
-    });
-
-    Aimbot.Toggle({
-        Text = "Team Check",
-        Callback = function(bool) 
-            Flags.TeamCheck = bool;
-        end
-    });
-
-    Aimbot.Dropdown({
-        Text = "Target Part",
-        Callback = function(value) 
-            Flags.AimbotTarget = value;
-        end,
-        Options = {
-            "Head",
-            "HumanoidRootPart"
-        }
-    });
-
-    GetClosestPlayer = function(novisible) 
-        if not LocalPlayer.Character or not FindFirstChild(LocalPlayer.Character, "HumanoidRootPart") then return end;
-    
-        local LPos = LocalPlayer.Character.HumanoidRootPart.Position;
-        local Players = {};
-    
-        for i,v in Pairs(GetChildren(GPlayers)) do
-            if v ~= LocalPlayer then
-                if Flags.TeamCheck and v.TeamColor == LocalPlayer.TeamColor then 
+            local Character = v.Character;
+            local HumanoidRootPart = Character and FindFirstChild(Character, "HumanoidRootPart");
+            if HumanoidRootPart then
+                if novisible then -- doesnt care about visibilty
+                    local Between = (HumanoidRootPart.Position - LPos).magnitude;
+                    Players[#Players+1] = {Between, v};
                     continue;
                 end;
 
-                local Character = v.Character;
-                local HumanoidRootPart = Character and FindFirstChild(Character, "HumanoidRootPart");
-                if HumanoidRootPart then
-                    if novisible then -- doesnt care about visibilty
-                        local Between = (HumanoidRootPart.Position - LPos).magnitude;
-                        Players[#Players+1] = {Between, v};
-                        continue;
-                    end;
-
-                    local _, Visible = WorldToViewportPoint(Camera, HumanoidRootPart.Position);
-                    if Visible then
-                        local Between = (HumanoidRootPart.Position - LPos).magnitude;
-                        Players[#Players+1] = {Between, v};
-                    end;
+                local _, Visible = WorldToViewportPoint(Camera, HumanoidRootPart.Position);
+                if Visible then
+                    local Between = (HumanoidRootPart.Position - LPos).magnitude;
+                    Players[#Players+1] = {Between, v};
                 end;
             end;
         end;
-    
-        sort(Players, function(x,y) return x[1] < y[1] end);
-    
-        return Players[1] and Players[1][2];
     end;
+
+    sort(Players, function(x,y) return x[1] < y[1] end);
+
+    return Players[1] and Players[1][2];
+end;
+
+-- Legit
+do 
+    local LegitTab = Window:Tab("Legit");
+
+    local Options = LegitTab:Section("Options");
+    Options:Toggle("Team Check", false, function(value) Flags.TeamCheck = value; end);
+
+    local RageBot = LegitTab:Section("Rage");
+    RageBot:Toggle("Enabled", false, function(value) Flags.RageBot = value; end);
+    RageBot:Toggle("Visible Check", false, function(value) Flags.RageVisible = value; end);
+    RageBot:Dropdown("Target", "Head", {"Head", "HumanoidRootPart"}, function(selected) Flags.RageTarget = selected; end);
+
+    local TriggerBot = LegitTab:Section("Trigger");
+    TriggerBot:Toggle("Enabled", false, function(value) Flags.TriggerBot = value; end);    
+    TriggerBot:Slider("Delay", 0, 10, 0, function(value) Flags.TriggerBotDelay = value end);
+
+    local Aimbot = LegitTab:Section("Aimbot");
+    Aimbot:Toggle("Enabled", false, function(value) Flags.Aimbot = value end);
+    Aimbot:Toggle("Visible Check", true, function(value) Flags.AimbotVisible = value; end);
+    Aimbot:Dropdown("Target", "Head", {"Head", "HumanoidRootPart"}, function(selected) Flags.AimbotTarget = selected; end);
+
+    local Weapon = LegitTab:Section("Weapon");
+    Weapon:Toggle("Infinite Ammo", false, function(value) Flags.InfiniteAmmo = value; end);
+    Weapon:Toggle("No Recoil", false, function(value) Flags.NoRecoil = value; end);
+    Weapon:Toggle("No Spread", false, function(value) Flags.NoSpread = value; end);
+
+    Flags.AimbotVisible = true;
+    Flags.TriggerBotDelay = 0;
+end;
+
+-- Player
+do 
+    local Player = Window:Tab("Player");
+    local Main = Player:Section("Main");
+
+    Main:Slider("WalkSpeed", 1, 200, LocalPlayer.Character.Humanoid.WalkSpeed, function(value) 
+        Flags.WalkSpeed = value;
+    end);
+
+    Main:Slider("JumpHeight", 1, 200, LocalPlayer.Character.Humanoid.JumpHeight, function(value) 
+        Flags.JumpHeight = value;
+    end);
+
+    local AntiAim = Player:Section("Anti Aim");
+    AntiAim:Toggle("Auto Crouch", false, function(value) Flags.AutoCrouch = value; end);
+    AntiAim:Slider("Delay", 0, 10, 0, function(value) Flags.AntiAimDelay = value; end);
+
+    Flags.AntiAimDelay = 0;
+end;
+
+-- Visual
+do 
+    local Visual = Window:Tab("Visual");
+    local Main = Visual:Section("Main");
+
+    Main:Slider("FOV", 1, 150, 70, function(value) Flags.FieldOfView = value; end);
+end;
+
+-- Misc
+do 
+    local Misc = Window:Tab("Misc");
+    local Main = Misc:Section("Main");
+
+    Main:Toggle("Staff Check", false, function(value) 
+        if value then
+            for i,v in Pairs(GetChildren(GPlayers)) do
+                if ModCheck.isMod(v) then
+                    SynapseNotification("A moderator is in your game!", ToastType.Warning);
+                end;
+            end;
+        end;
+        Flags.ModCheck = value;
+    end);
 end;
 
 local Mouse = LocalPlayer:GetMouse();
 
+game.Players.PlayerAdded:Connect(function(player) 
+    if Flags.ModCheck and ModCheck.isMod(player) then
+        SynapseNotification("A moderator is in your game!", ToastType.Warning);
+    end;
+end);
+
 game.RunService.RenderStepped:Connect(function() 
+    if equipped.Value == "none" then return end;
+
     if Flags.Aimbot and IsMouseButtonPressed(UserInputService, MouseButton2) then
-        local Closest = GetClosestPlayer();
+        local Closest = GetClosestPlayer(not Flags.AimbotVisible);
         if Closest then
             local Target = Flags.AimbotTarget or "Head";
             Camera.CFrame = CFrame(Camera.CFrame.Position, Closest.Character[Target].Position);
         end;
     end;
 
-    if Flags.Triggerbot then
-        if not Mouse.Target or not Mouse.Target.Parent then return end;
-
-        local Parent = Mouse.Target.Parent;
-        local Player = FindFirstChild(GPlayers, Parent.Name);
-        if not Player or Player == LocalPlayer or Flags.TeamCheck and Player.TeamColor == LocalPlayer.TeamColor then return end;
-
-        Weapon.firebullet();
+    if Flags.TriggerBot then
+        if Mouse.Target and Mouse.Target then
+            local Head = LocalPlayer.Character.Head.Position;
+            local Params = RaycastParams();
+            Params.FilterDescendantsInstances = { LocalPlayer.Character, Camera, Workspace.Map.Ignore };
+            
+            local Result = Raycast(Workspace, Head, Mouse.Target.Position - Head, Params);
+            if Result and FindFirstChild(GPlayers, Result.Instance.Parent.Name) then
+                local Player = FindFirstChild(GPlayers, Result.Instance.Parent.Name);
+                -- Cannot return due to it not reaching other FLAGS!
+                if Flags.TeamCheck then
+                    if Player.TeamColor ~= LocalPlayer.TeamColor then
+                        Delay(Flags.TriggerBotDelay, Weapon.firebullet);
+                    end;
+                else
+                    Delay(Flags.TriggerBotDelay, Weapon.firebullet);
+                end;
+            end;
+        end;
     end;
 
-    if Flags.Ragebot then
-        local Closest = GetClosestPlayer(true); -- will be able to change when we have the new ui
-        if not Closest or not FindFirstChild(LocalPlayer.Character, "HumanoidRootPart") then return end;
-        
-        local Head = LocalPlayer.Character.Head.Position;
-        local Params = RaycastParams();
-        Params.FilterDescendantsInstances = { LocalPlayer.Character, Camera };
+    if Flags.RageBot and FindFirstChild(LocalPlayer.Character, "HumanoidRootPart") then
+        local Closest = GetClosestPlayer(not Flags.RageVisible);
 
-        local Result = Raycast(Workspace, Head, Closest.Character.HumanoidRootPart.Position - Head, Params);
-        if Result and IsDescendantOf(Result.Instance, Closest.Character) then
-            Camera.CFrame = CFrame(Camera.CFrame.Position, Closest.Character.Head.Position);
-            Weapon.firebullet();
+        if Closest then
+            local Head = LocalPlayer.Character.Head.Position;
+            local Params = RaycastParams();
+            Params.FilterDescendantsInstances = { LocalPlayer.Character, Camera, Workspace.Map.Ignore };
+    
+            local Target = Closest.Character[Flags.RageTarget or "Head"].Position;
+            local Result = Raycast(Workspace, Head, Target - Head, Params);
+            if Result and IsDescendantOf(Result.Instance, Closest.Character) then
+                Camera.CFrame = CFrame(Camera.CFrame.Position, Target);
+                Weapon.firebullet();
+            end;
         end;
     end;
 
     if Flags.AutoCrouch then
         local ctrlcrouch = Crouch.ctrlcrouch; 
-        ctrlcrouch.Value = not ctrlcrouch.Value;
+        Delay(Flags.AntiAimDelay, function() 
+            ctrlcrouch.Value = not ctrlcrouch.Value;
+        end);
     end;
 end);
 
