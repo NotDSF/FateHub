49d8f96232a6f83c4713320ab38788864011360a
NotDSF <67711164+NotDSF@users.noreply.github.com>
2022-04-10 19:34:53 +0100

    Create Import.lua

---
+++ Import.lua
@@ -0,0 +1,27 @@
+local Import do
+    local request = syn.request;
+    local format = string.format;
+    local HttpService = game.HttpService;
+    local decode = syn.crypt.base64.decode;
+
+    Import = function(name) 
+        local Response = request({
+            Url = format("https://api.github.com/repos/NotDSF/FateHub/contents/%s", name),
+            Headers = {
+                Authorization = "bearer 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"
+            }
+        });
+
+        if Response.Status ~= 200 then
+            error(format("Import fetch failed: https://api.github.com/repos/NotDSF/FateHub/contents/%s", name));
+        end;
+
+        local ResponseBody = HttpService.JSONDecode(HttpService, Response.Body);
+        local res, err = loadstring(decode(ResponseBody.content));
+        if not res then
+            return err;
+        end;
+
+        return res();
+    end;
+end;
