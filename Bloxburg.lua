117fe145d5c2c820b8d76c771d572a34e6fa0e25
fate <sulieguy81@gmail.com>
2022-08-06 17:01:05 +0100

    finish ben icecream farm

--- Bloxburg.lua
+++ Bloxburg.lua
@@ -1,3 +1,197 @@
+-- yes  i am gonna rewrite this, i am just rushing it all
+
+local RunService = game:GetService("RunService")
+local UILibrary = loadfile("UILibrary.lua")();
+local VisualsLib = loadfile("Visuals.lua")();
+local Visuals = VisualsLib.new();
+local Services = setmetatable({}, {
+    __index = function(self, serviceName)
+        local good, service = pcall(game.GetService, game, serviceName);
+        if (good) then
+            self[serviceName] = service
+            return service;
+        end
+    end
+});
+
+local Workspace = Services.Workspace
+local currentCamera = Workspace.currentCamera
+local worldtoscreen = currentCamera.WorldToScreenPoint
+local viewportSize = currentCamera.ViewportSize
+local ReplicatedStorage = Services.ReplicatedStorage
+local UserInputService = Services.UserInputService
+local TweenService = Services.TweenService
+local Players = Services.Players
+local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse();
+local mouseVector = Vector2.new(Mouse.X, Mouse.Y);
+local inset = Services.GuiService:GetGuiInset();
+
+local findFirstChild = game.FindFirstChild
+local waitForChild = game.WaitForChild
+local findPartOnRayWithIgnoreList = Workspace.FindPartOnRayWithIgnoreList
+local GetPartsObscuringTarget = currentCamera.GetPartsObscuringTarget
+
+local cframenew = CFrame.new
+local cframe_angles = CFrame.Angles
+local vector3new = Vector3.new
+local vector2new = Vector2.new
+local color3new = Color3.new
+local fromRGB = Color3.fromRGB
+local wait = task.wait
+local split = string.split
+local format = string.format
+local floor = math.floor
+local rad = math.rad
+local random = math.random
+local create = table.create
+local tfind = table.find
+local insert = table.insert
+local IsA = game.IsA
+
+local protectedInstances = {};
+local keys = {};
+
+local settings = {
+    auto_farm = {
+        ben = false
+    }
+};
+
+local trampoline_call = function(func, ...)
+    local funcinfo = debug.getinfo(func);
+    local funcenv = getfenv(func);
+    local result = {syn.trampoline_call(func, {
+        env = funcenv,
+        source = funcinfo.source,
+        name = funcinfo.name,
+        currentline = funcinfo.currentline,
+        numparams = funcinfo.numparams,
+        is_varag = funcinfo.is_vararg
+    }, {
+        identity = 2,
+        script = script,
+        env = funcenv
+    }, ...)}
+    if (result[1]) then
+        return unpack(result, 2);
+    end
+    error("trampoline call error:\n" .. debug.traceback());
+end
+
+
+local protectInstance = function(instance)
+    protectedInstances[#protectedInstances + 1] = instance
+end
+
+local tp_to = function(pos)
+    local char = LocalPlayer.Character
+    if (char) then
+        local root = char.HumanoidRootPart
+        root.CFrame = pos
+        return true;
+    end
+    return false
+end
+
+local tweento = function(pos, noyield)
+    local char = LocalPlayer.Character
+    if (char) then
+        local root = char.HumanoidRootPart
+        local distance = (root.Position - pos.Position).Magnitude
+        local tweenInfo = TweenInfo.new(distance / 250 * settings.tween_speed, Enum.EasingStyle.Linear, Enum.EasingDirection.Out);
+        local tween = TweenService:Create(root, tweenInfo, { CFrame = pos });
+        tween:Play();
+        if (not noyield) then
+            tween.Completed:Wait();
+        end
+        return true;
+    end
+    return false
+end
+
+local serverhop = function(order)
+    local Servers = {};
+    local url = string.format("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=100", game.PlaceId, order);
+    local starting = tick();
+    local Server;
+    repeat
+        local good, result = pcall(function()
+            return game:HttpGet(url);
+        end);
+        if (not good) then
+            wait(2);
+            continue;
+        end
+        local decoded = Services.HttpService:JSONDecode(result);
+        if (#decoded.data ~= 0) then
+            Servers = decoded.data
+            for i, v in pairs(Servers) do
+                if (v.maxPlayers and v.playing and v.maxPlayers > v.playing) then
+                    Server = v
+                    break;
+                end
+            end
+            if (Server) then
+                break;
+            end
+        end
+        url = format("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=100&cursor=%s", game.PlaceId, order, decoded.nextPageCursor);
+    until tick() - starting >= 600;
+    if (not Server or #Servers == 0) then
+        return "no servers found";
+    end
+
+    local queue_on_teleport = syn and syn.queue_on_teleport or queue_on_teleport
+    if (queue_on_teleport) then
+        queue_on_teleport("print('scripthere')");
+    end;
+
+    Services.TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id);
+end
+
+local flyChar = function(camera, root, speed)
+    local bodyGyro = Instance.new("BodyGyro");
+    local bodyVelocity = Instance.new("BodyVelocity");
+
+    protectInstance(bodyGyro);
+    protectInstance(bodyVelocity);
+    bodyGyro.Parent = root
+    bodyVelocity.Parent = root
+    bodyGyro.P = 9e9
+    bodyGyro.MaxTorque = Vector3.one * 9e9
+    bodyGyro.CFrame = root.CFrame
+    bodyVelocity.MaxForce = Vector3.one * 9e9
+    bodyVelocity.Velocity = Vector3.zero
+
+    local veczero = Vector3.zero
+
+    local WASD = { W = 0, A = 0, S = 0, D = 0 };
+    camera = camera or currentCamera
+    speed = math.clamp(speed or 3, 1, 7);
+    task.spawn(function()
+        settings.character.flying = true
+        while (settings.character.flying) do
+            WASD.W = keys.W and speed or 0
+            WASD.A = keys.A and -speed or 0
+            WASD.S = keys.S and -speed or 0
+            WASD.D = keys.D and speed or 0
+            if ((WASD.W + WASD.S) ~= 0 or (WASD.A + WASD.D) ~= 0) then
+                bodyVelocity.Velocity = ((camera.CoordinateFrame.lookVector * (WASD.W + WASD.S)) + ((camera.CoordinateFrame * CFrame.new(WASD.A + WASD.D, (WASD.W + WASD.S) * 0.2, 0).Position) - camera.CoordinateFrame.Position)) * 50
+            else
+                bodyVelocity.Velocity = veczero
+            end
+            bodyGyro.CFrame = camera.CoordinateFrame
+            wait();
+        end
+        bodyVelocity:Destroy();
+        bodyGyro:Destroy();
+    end);
+end
+
+local Environment = Workspace:WaitForChild("Environment");
+local Locations = Environment:WaitForChild("Locations");
+
 local network = filtergc("table", {
     Keys = {"net"}
 }, true);
@@ -5,28 +199,81 @@ local network = filtergc("table", {
 local JobHandler = filtergc("table", {
     Keys = {"GoToWork"}
 }, true);
-
-local BensIceCream = filtergc("table", {
-    Keys = {"GetOrderPhrase", "AddCustomer", "ShiftRadius"}
+local LoadingHandler = filtergc("table", {
+    Keys = {"ShowLoading", "HideLoading"}
 }, true);
 
-JobHandler:GoToWork("BensIceCreamSeller");
+local fireServer = function(t)
+    local getRemote = debug.getupvalue(network.net.FireServer, 4);
+    local remote = getRemote(t.Type);
+    t.Type = nil
+    remote:FireServer(t);
+end
 
-
-local ChatBarEvent = Instance.new("BindableEvent");
+local jobsLoaded = false
+local jobsLoadedEvent = Instance.new("BindableEvent");
+local ChatBubbleEvent = Instance.new("BindableEvent");
 
 restorefunction(JobHandler.CreateChatBubble);
+restorefunction(LoadingHandler.ShowLoading);
+restorefunction(LoadingHandler.HideLoading);
 
-local old;
-old = hookfunction(JobHandler.CreateChatBubble, function(...)
-    local ret = old(...);
-    ChatBarEvent:Fire(ret);
+local oldCreateChatBubble;
+oldCreateChatBubble = hookfunction(JobHandler.CreateChatBubble, function(...)
+    local ret = oldCreateChatBubble(...);
+    if (settings.auto_farm.ben) then
+        print("firing chatbubble event");
+        ChatBubbleEvent:Fire(ret);
+    end
     return ret;
 end);
+local oldShowLoading;
+oldShowLoading = hookfunction(LoadingHandler.ShowLoading, function(...)
+    jobsLoaded = false
+    print("loading...");
+    return oldShowLoading(...);
+end);
 
+local oldHideLoading;
+oldHideLoading = hookfunction(LoadingHandler.HideLoading, function(...)
+    jobsLoaded = true
+    jobsLoadedEvent:Fire()
+    return oldHideLoading(...);
+end);
 
-ChatBarEvent.Event:Connect(function(chatBar)
-    network.net:FireServer({
+local flavours = {
+    ["Sunrise"] = {"Strawberry", { 936, 13, 1051 }},
+    ["Pine Cone"]  =  {"Chocolate", { 933, 13, 1052 }},
+    ["Pastel yellow"] = {"Vanilla", { 932, 13, 1051 }}
+};
+local toppings = {
+    ["rbxassetid://2132313170"] = {"Sprinkles", { 927, 13, 1047 }},
+    ["rbxassetid://455528053"] = {"Caramel", { 927, 13, 1045 }},
+    ["rbxassetid://22360490"] = {"Nuts", { 927, 13, 1043 }}
+};
+
+ChatBubbleEvent.Event:Connect(function(chatBar)
+    if (not jobsLoaded) then
+        print("loading job...");
+        jobsLoadedEvent.Event:Wait();
+        task.wait(1);
+        print("loaded");
+    end
+
+    local char = LocalPlayer.Character
+    local root = char:FindFirstChild("HumanoidRootPart");
+    if (not root) then end
+    local cupsPosition = CFrame.new(929, 13, 1050);
+    if ((root.Position - cupsPosition.Position).Magnitude > 5) then
+        tp_to(cupsPosition);
+        task.wait(.5);
+    end
+
+    local BensIceCream = filtergc("table", {
+        Keys = {"GetOrderPhrase", "AddCustomer", "ShiftRadius"}
+    }, true);
+
+    fireServer({
         Type = "TakeIceCreamCup"
     });
 
@@ -35,13 +282,79 @@ ChatBarEvent.Event:Connect(function(chatBar)
 
     for i, item in pairs(descendants) do
         if (item.Name == "Icon" and string.match(item.Parent.Name, "%s")) then
-            stuff[#stuff + 1] = item
+            ingredients[#ingredients + 1] = item
         end
     end
 
+    local fillFlavour = debug.getproto(BensIceCream.StartShift, 2, true)[1]
+    local fillTopping = debug.getproto(BensIceCream.StartShift, 3, true)[1]
+
     for i, item in pairs(ingredients) do
         if (item.Image == "rbxassetid://2132263837") then -- ice cream flavour
-            
+            local ImageColor3 = item.ImageColor3
+            local Brick = BrickColor.new(ImageColor3);
+            for color, flavour in pairs(flavours) do
+                if (color == tostring(Brick)) then
+                    tp_to(CFrame.new(unpack(flavour[2])));
+
+                    debug.setupvalue(fillFlavour, 3, flavour[1]);
+                    debug.setupvalue(fillFlavour, 5, flavour[1]);
+                    local doFillFlavour = select(3, fillFlavour());
+
+                    if (doFillFlavour) then
+                        print("Adding " .. flavour[1]);
+                        debug.setupvalue(doFillFlavour, 3, flavour[1]);
+                        debug.setupvalue(doFillFlavour, 5, flavour[1]);
+                        doFillFlavour();
+                    end
+                end
+            end
+        elseif (toppings[item.Image]) then
+            local toppingData = toppings[item.Image]
+            tp_to(CFrame.new(unpack(toppingData[2])));
+
+            debug.setupvalue(fillTopping, 3, toppingData[1]);
+            debug.setupvalue(fillTopping, 5, toppingData[1]);
+            local doFillTopping = select(3, fillTopping());
+            if (doFillTopping) then
+                print("Adding " .. toppingData[1]);
+                debug.setupvalue(doFillTopping, 3, toppingData[1]);
+                debug.setupvalue(doFillTopping, 5, toppingData[1]);
+                task.wait(.75);
+                doFillTopping();
+            end
+        end
+        task.wait(.1);
+    end
+
+    local customerModel = chatBar:FindFirstAncestor("BensIceCreamCustomer");
+    local customerRoot = customerModel:WaitForChild("HumanoidRootPart");
+    tp_to(customerRoot.CFrame * CFrame.new(0, 0, -5));
+    root.CFrame *= CFrame.Angles(0, math.pi, 0);
+
+    local workStations = workspace.Environment.Locations.BensIceCream.CustomerTargets:GetChildren();
+    local workStation;
+    for i , _workStation in pairs(workStations) do
+        if (_workStation.Occupied.Value == customerModel) then
+            workStation = _workStation
+            break;
         end
     end
+    fireServer({
+        Type = "JobCompleted",
+        Workstation = workStation
+    });
+end);
+
+
+local mainWindow = UILibrary:CreateWindow("Fate Hub", "Blox Fruits", Color3.fromRGB(100, 0, 255));
+
+local main = mainWindow:Tab("Main");
+local autoFarm = main:Section("Auto Farm");
+
+autoFarm:Toggle("Auto Ben", settings.auto_farm.ben, function(callback)
+    if (callback) then
+        JobHandler:GoToWork("BensIceCreamSeller");
+    end
+    settings.auto_farm.ben = callback
 end);
